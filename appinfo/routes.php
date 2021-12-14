a1349f06f66cee33429145961cbef8e2cf24b0ad
jimmyl0l3c <23386405+jimmyl0l3c@users.noreply.github.com>
2021-12-14 15:56:15 +0100

    minor changes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,15 +1,9 @@
 <?php
 
 return [
-	'resources' => [
-		'note' => ['url' => '/notes'],
-		'note_api' => ['url' => '/api/0.1/notes']
-	],
 	'routes' => [
 		['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
         ['name' => 'share#create', 'url' => '/new', 'verb' => 'POST'],
         ['name' => 'share#update', 'url' => '/update', 'verb' => 'POST'],
-//		['name' => 'note_api#preflighted_cors', 'url' => '/api/0.1/{path}',
-//			'verb' => 'OPTIONS', 'requirements' => ['path' => '.+']]
 	]
 ];
