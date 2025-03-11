72e6df9a5ade69dcd7645bc2964290de3b3ebbd6
Filip Joska <filip@joska.dev>
2025-03-11 21:58:34 +0100

    Fix renaming link

--- src/reg_rename_link.ts
+++ src/reg_rename_link.ts
@@ -5,6 +5,7 @@ console.info('CfgShareLinks: RenameLink init')
 // Add rename input
 window.addEventListener('DOMContentLoaded', () => {
 	if (OCA.Sharing && OCA.Sharing.ExternalShareActions) {
+		console.info('CfgShareLinks: Registering rename action')
 		OCA.Sharing.ExternalShareActions.registerAction(new RenameShareAction())
 	}
 })
